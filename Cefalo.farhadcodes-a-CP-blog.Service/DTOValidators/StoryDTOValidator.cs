2e89eede6e6f35ec3078fe721114ab6ae5be3221
frdhsn <farhadiitju@gmail.com>
2022-09-30 10:21:16 +0600

    validation done #9

---
+++ Cefalo.farhadcodes-a-CP-blog.Service/DTOValidators/StoryDTOValidator.cs
@@ -0,0 +1,25 @@
+﻿using Cefalo.farhadcodes_a_CP_blog.Service.DTO.Story;
+using Cefalo.TechDaily.Service.DtoValidators;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cefalo.farhadcodes_a_CP_blog.Service.DTOValidators
+{
+    public class StoryDTOValidator: BaseDTOValidator<StoryDTO>
+    {
+        public StoryDTOValidator()
+        {
+
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title can not be empty!");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description can not be empty!");
+            RuleFor(x => x.Topic).NotEmpty().WithMessage("Topic can not be empty!");
+            RuleFor(x => x.Difficulty).NotEmpty().WithMessage("Difficulty can not be empty!");
+            RuleFor(x => x.AuthorID).NotEmpty().WithMessage("AuthorID can not be empty!");
+        }
+
+    }
+}
---
+++ Cefalo.farhadcodes-a-CP-blog.Service/DTOValidators/UserDTOValidator.cs
@@ -0,0 +1,22 @@
+﻿using Cefalo.farhadcodes_a_CP_blog.Service.DTO.User;
+using Cefalo.TechDaily.Service.DtoValidators;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cefalo.farhadcodes_a_CP_blog.Service.DTOValidators
+{
+    public class UserDTOValidator: BaseDTOValidator<UserDTO>
+    {
+        public UserDTOValidator()
+        {
+
+            RuleFor(x => x.Name).NotEmpty().WithMessage("User name is required.");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required.");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Invalid email format.");
+        }
+    }
+}
